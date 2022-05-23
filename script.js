0e53495371e351bf6d02ada11c392e5d42949dd2
pblanco1986 <106123812+pblanco1986@users.noreply.github.com>
2022-05-23 18:57:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+<script>
+
+          let popup = document.getElementById("popup");
+
+      function abrirPopup(){
+
+        popup.classList.add("open-popup");
+      }
+      
+      function cerrarPopup(){
+        
+        popup.classList.remove("open-popup");
+        
+      }
